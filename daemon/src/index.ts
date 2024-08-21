41e2608133da2425644fa855970bed69ac2862c4
EvilsSouls <evilssouls@protonmail.com>
2024-08-20 20:26:32 -0600

    Added first basic Reading functionality

--- daemon/src/index.ts
+++ daemon/src/index.ts
@@ -1,3 +1,30 @@
 const { Console } = require("node:console");
 
-console.log("Hello World!");
+const robot = require("kbm-robot");
+
+const { SerialPort } = require('serialport');
+const { ReadlineParser } = require('@serialport/parser-readline');
+const port = new SerialPort({ path: '/dev/ttyACM0', baudRate: 9600});
+
+interface Data {
+    x: number;
+    y: number;
+    pressed: boolean;
+}
+
+const parser = port.pipe(new ReadlineParser({ delimiter: '\t' }));
+parser.on('data', (rawData: string) => {
+    const formattedData: Data = JSON.parse(rawData);
+});
+
+robot.startJar();
+
+robot.press("alt")
+    .press("tab")
+    .sleep(100)
+    .release("tab")
+    .release("alt")
+    .sleep(100)
+    .typeString("Hello World!")
+    .go()
+    .then(robot.stopJar);
