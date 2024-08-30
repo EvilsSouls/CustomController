640413466d38ef95b5f4c831797746b5861eba0e
Zteid7464 <thokabafi69@gmain.com>
2024-08-30 07:34:08 -0600

    Added stuff

--- controller-app/main.py
+++ controller-app/main.py
@@ -1,5 +1,6 @@
 from picozero import pico_temp_sensor, pico_led
 import utime
+import json
 from machine import Pin, ADC
 
 xAxis = ADC(Pin(27))
@@ -9,9 +10,11 @@ button = Pin(16, Pin.IN, Pin.PULL_UP)
 
 
 while True:
+    # str(bool(abs(button_value-1)))
     xValue = xAxis.read_u16() // 10
     yValue = yAxis.read_u16() // 10
     button_value = button.value()
-    print(str({"x": xValue, "y": yValue, "pressed": str(bool(abs(button_value-1))})), end="\t")
+    data = {"x": {xValue}, "y": {yValue}, "pressed": str(bool(abs(button_value-1)))}
+    print(json.dumps(data).replace("True", "true").replace("False", "false"), end="hello")
     utime.sleep(0.1)
 
